eb70b21339c74434351b665096390623c9e7cdf6
quangthanhngo <romaisonngo@gmail.com>
2021-08-10 17:32:54 +0700

    add Model (2nd commit)

--- CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,7 @@ namespace CmsShoppingCart.Infrastructure
         {
 
         }
+
+        public DbSet<Page> Pages { get; set; }
     }
 }
